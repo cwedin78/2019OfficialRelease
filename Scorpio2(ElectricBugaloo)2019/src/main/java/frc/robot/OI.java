94a1616f9912614dddecff8038b9752d30f9e35b
iansaw16 <iwalkblood@gmail.com>
2019-02-05 17:00:37 -0600

    created controllers

--- Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/OI.java
+++ Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/OI.java
@@ -7,11 +7,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+  public Joystick driver, operator;
+
+
+  public OI(){
+
+    driver = new Joystick(0);
+    operator = new Joystick(1);
+
+  }
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
