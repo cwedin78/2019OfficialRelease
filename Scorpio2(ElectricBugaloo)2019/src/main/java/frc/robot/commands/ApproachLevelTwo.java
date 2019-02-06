e3878b1cc86158b2aea7d37c528a18f4575842c1
iansaw16 <iwalkblood@gmail.com>
2019-02-05 20:48:10 -0600

    enacted a rough estimate for getting the elevator to level 2 rocket hatches

---
+++ Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/commands/ApproachLevelTwo.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class ApproachLevelTwo extends Command {
+
+  public double error, kp, kd, setpoint;
+  public ApproachLevelTwo() {
+    requires(Robot.lift);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+
+    kp = .05;
+    kd = .03;
+
+    setpoint = 37.333333333; //closest estimate as of now
+
+    error = setpoint - Robot.lift.liftencoder.getPosition();
+
+
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+
+    Robot.lift.lift.set(Robot.lift.PIDSpeed(kp, kd, error));
+
+
+    
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
