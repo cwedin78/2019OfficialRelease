2580faaee398ace618005c30b387a9dff41f67ce
iansaw16 <iwalkblood@gmail.com>
2019-02-06 16:54:23 -0600

    added a way to get rid of hatches after we raise the elevator

--- Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/subsystems/Elevator.java
+++ Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,10 +7,12 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
@@ -20,9 +22,9 @@ public double value, last_error;
 
 public CANSparkMax lift;
 
-
 public CANEncoder liftencoder;
 
+  
   public Elevator(){
 
     lift = new CANSparkMax(8, MotorType.kBrushless);
