35664cd7fe925581e45fe195b633795c1ecb2977
iansaw16 <iwalkblood@gmail.com>
2019-02-08 15:55:07 -0600

    missed a save

--- Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/Robot.java
+++ Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/Robot.java
@@ -12,10 +12,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.CenterTarget;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.LimeLight;
 
