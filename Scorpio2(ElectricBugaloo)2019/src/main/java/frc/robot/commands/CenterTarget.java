c13c4314401040b98f6766e6a8ebe4b2c70f0960
nikolai <nikolai.solo.b@gmail.com>
2019-02-09 10:22:29 -0600

    fixed conflicts post-merge

--- Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/commands/CenterTarget.java
+++ Scorpio2(ElectricBugaloo)2019/src/main/java/frc/robot/commands/CenterTarget.java
@@ -29,7 +29,7 @@ public class CenterTarget extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    forward = Robot.drivetrain.CalculateControllerValue(.3, .25, .75, Robot.m_oi.driver, false, "y");
+    forward = Robot.drivetrain.CalculateControllerValue(.3, .25, .75, Robot.m_oi.driver, false, "Y");
 
     correction = Robot.drivetrain.PIDSpeed(kP, kD, Robot.limelight.tx.getDouble(0.0 * -1));
 
